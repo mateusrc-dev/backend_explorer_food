a227e449d3673772811e2cb1edef27fab3d25079
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-10-23 18:33:51 -0300

    updated backend

--- src/Controllers/AllRequestsController.js
+++ src/Controllers/AllRequestsController.js
@@ -1,13 +1,27 @@
 const knex = require("../database/knex")
+const sqliteConnection = require("../database/sqlite")
 
 class AllRequestsController {
   async create(request, response) {
     const { details, status } = request.body
     const { user_id } = request.params
     await knex("allRequests").insert({ details, status, user_id })
-    response.json()
+    return response.json()
   }
-  
+  async update(request, response) {
+    const { status } = request.body
+    const { id } = request.params;
+    const database = await sqliteConnection()
+    const requests = await database.get("SELECT * FROM allRequests WHERE id = (?)", [id])
+    requests.status = status
+    await database.run(`
+    UPDATE allRequests SET 
+    status = ?
+    WHERE id = ?`, 
+    [requests.status, id])
+    return response.json(requests)
+  }
+
   async delete(request, response) {
     const { id } = request.params
     await knex("allRequests").where({ id }).delete()
